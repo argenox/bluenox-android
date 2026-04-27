43ae4ab2b245b6ca15891fc18253ba37a7008cf5
Gustavo Litovsky <gustavo@argenox.com>
2026-04-26 20:44:23 -0500

    build: updates build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     `maven-publish`
+    signing
 }
 
 group = "com.argenox"
-version = "0.2.52"
+version = (findProperty("VERSION_NAME") as String?) ?: "0.2.52"
 
 android {
     namespace = "com.argenox.bluenoxandroid"
@@ -36,6 +37,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
 }
 
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
 publishing {
     publications {
         create<MavenPublication>("release") {
@@ -45,13 +50,51 @@ publishing {
             afterEvaluate {
                 from(components["release"])
             }
+            artifact(javadocJar)
             pom {
                 name.set("BlueNox Android")
                 description.set("BlueNox BLE Android library")
+                url.set("https://github.com/argenox/bluenox-android")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("argenox")
+                        name.set("Argenox")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/argenox/bluenox-android.git")
+                    developerConnection.set("scm:git:ssh://github.com:argenox/bluenox-android.git")
+                    url.set("https://github.com/argenox/bluenox-android")
+                }
             }
         }
     }
     repositories {
+        maven {
+            name = "mavenCentral"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = (findProperty("ossrhUsername") as String?) ?: System.getenv("OSSRH_USERNAME")
+                password = (findProperty("ossrhPassword") as String?) ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
         mavenLocal()
+    }
+}
+
+signing {
+    val signingKey = (findProperty("signingInMemoryKey") as String?) ?: System.getenv("GPG_SIGNING_KEY")
+    val signingPassword = (findProperty("signingInMemoryKeyPassword") as String?) ?: System.getenv("GPG_PASSWORD")
+    if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications)
     }
 }
